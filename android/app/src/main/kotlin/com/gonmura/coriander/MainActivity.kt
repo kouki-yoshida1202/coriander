f9961f621516d6207650a15178c58df01073d5ef
yoshidako <u750186j@outlook.jp>
2020-10-28 16:07:50 +0900

    providerModel

---
+++ android/app/src/main/kotlin/com/gonmura/coriander/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gonmura.coriander
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:coriander/main_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("コリアンダー"),
+          ),
+          body: Consumer<MainModel>(
+            builder: (context, model, child) {
+              return Center(
+                child: Column(
+                  children: [
+                    Text(model.kboyText),
+                    RaisedButton(
+                      child: Text("ボタン"),
+                      onPressed: () {
+                        model.changeKboyText();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
